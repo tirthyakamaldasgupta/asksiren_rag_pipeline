1a3dce2a503fd0b1d981bd96fcacc550950e4098
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2025-07-16 09:55:27 +0530

    chore: Updated requirements.txt and added .gitignore.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import os
+
+import bs4
 from dotenv import load_dotenv
+from langchain import hub
 from langchain.chat_models import init_chat_model
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_mistralai import MistralAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def main():
+    question = input("What is your question? ")
+
     llm = init_chat_model(
         "mistral-large-latest",
         model_provider="mistralai"
@@ -14,9 +22,37 @@ def main():
 
     vector_store = InMemoryVectorStore(embeddings)
 
-    response = llm.invoke("Hi. Good morning. How are you today?")
+    prompt = hub.pull("rlm/rag-prompt")
 
-    print(response.content)
+    loader = WebBaseLoader(
+        web_paths=(os.environ["SIRENCRAFTBREW_FAQS_URL"],),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("title", "faqAnswer")
+            )
+        ),
+    )
+
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=20
+    )
+
+    all_splits = text_splitter.split_documents(docs)
+
+    _ = vector_store.add_documents(documents=all_splits)
+
+    retrieved_docs = vector_store.similarity_search(question)
+
+    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+    messages = prompt.invoke({"question": question, "context": docs_content})
+
+    response = llm.invoke(messages)
+
+    print(response.content.replace(". ", ".\n"))
 
 
 load_dotenv()
